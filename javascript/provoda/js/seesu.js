097c490a0c5568c3002861c0fe74c527179c9f3e
Gleb Arestov <gr1b0k@gmail.com>
2014-06-01 11:17:31 +0400

    ver up

--- javascript/provoda/js/seesu.js
+++ javascript/provoda/js/seesu.js
@@ -9,7 +9,7 @@ ScApi, torrent_searches, FuncsQueue, LastfmAPIExtended,
 AppModel, comd, LfmAuth, StartPage, SeesuServerAPI, VkAuth, VkApi, initVk,
 PlayerSeesu, invstg, cache_ajax, ProspApi, VkRawSearch) {
 'use strict';
-var app_version =  3.37;
+var app_version =  3.38;
 
 var
 	localize = app_serv.localize,
